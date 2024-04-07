3f9441795a56e0d38c7518d570fcd224d4531239
Anuj Mourya <136978647+anuj0224@users.noreply.github.com>
2024-04-07 20:27:44 +0530

    Update Contact.jsx

--- src/pages/Contacts/Contact.jsx
+++ src/pages/Contacts/Contact.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 function Contact() {
+  const [formData, setFormData] = useState({ name: '', email: '', message: '' });
+
   const onSubmit = async (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
-
     formData.append("access_key", "e33b3ef5-56f4-49f8-825b-8de2c559fc91");
 
     const object = Object.fromEntries(formData);
@@ -21,24 +22,27 @@ function Contact() {
 
     if (res.success) {
       console.log("Success", res);
+      // Clear the form fields
+      setFormData({ name: '', email: '', message: '' });
     }
   };
+
   return (
     <section className='contact-form'>
-        <header>
-            <h3 className='h3 form-title'>Contact Form</h3>
-        </header>
-         <form onSubmit={onSubmit} className='form'>
-            <div className='input-wrapper' >
-                <input type='text' name='fullname' id='fullname' className='form-input' placeholder='Full Name' />
-                <input type='email' name='email' id='email' placeholder='Email Address' className='form-input'/>
-            </div>
-            <textarea name='message' className='form-input' placeholder='Your Messages'></textarea>
-            <button type='submit' className='form-btn'>
-                <span>Send Message</span></button>
-         </form>    
+      <header>
+        <h3 className='h3 form-title'>Contact Form</h3>
+      </header>
+      <form onSubmit={onSubmit} className='form'>
+        <div className='input-wrapper'>
+          <input type='text' name='fullname' id='fullname' className='form-input' placeholder='Full Name' value={formData.name} onChange={(e) => setFormData({ ...formData, name: e.target.value })} />
+          <input type='email' name='email' id='email' placeholder='Email Address' className='form-input' value={formData.email} onChange={(e) => setFormData({ ...formData, email: e.target.value })} />
+        </div>
+        <textarea name='message' className='form-input' placeholder='Your Messages' value={formData.message} onChange={(e) => setFormData({ ...formData, message: e.target.value })}></textarea>
+        <button type='submit' className='form-btn'>
+          <span>Send Message</span></button>
+      </form>
     </section>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
