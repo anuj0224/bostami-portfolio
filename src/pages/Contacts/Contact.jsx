6b9d967ffa25503328cd9b24762d126818a38f2f
Anuj Mourya <136978647+anuj0224@users.noreply.github.com>
2024-04-07 19:21:38 +0530

    Update Contact.jsx

--- src/pages/Contacts/Contact.jsx
+++ src/pages/Contacts/Contact.jsx
@@ -1,34 +1,12 @@
 import React from 'react'
 
 function Contact() {
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-
-    formData.append("access_key", "e33b3ef5-56f4-49f8-825b-8de2c559fc91");
-
-    const object = Object.fromEntries(formData);
-    const json = JSON.stringify(object);
-
-    const res = await fetch("https://api.web3forms.com/submit", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json"
-      },
-      body: json
-    }).then((res) => res.json());
-
-    if (res.success) {
-      console.log("Success", res);
-    }
-  };
   return (
     <section className='contact-form'>
         <header>
             <h3 className='h3 form-title'>Contact Form</h3>
         </header>
-         <form onSubmit={onSubmit} className='form'>
+         <form className='form'>
             <div className='input-wrapper' >
                 <input type='text' name='fullname' id='fullname' className='form-input' placeholder='Full Name' />
                 <input type='email' name='email' id='email' placeholder='Email Address' className='form-input'/>
