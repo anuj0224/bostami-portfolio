7fa66f848dc779575ecf51e8660370b1a9d949f5
Anuj Mourya <136978647+anuj0224@users.noreply.github.com>
2025-05-14 18:51:35 +0530

    Update Resume.jsx

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -41,19 +41,25 @@ function Resume() {
             </div>
             <ol className='timeline-list'>
                 <TimelineItem title="Self-Working"
-                date = "2023 - Present" 
+                date = "2023 - 2024" 
                 description = "Working On Some Projects. You may See them in Portfolio Section.."/>
 
-                <TimelineItem title="Tutor"
-                date = "2020 - Present" 
-                description = "Teaching Science to Students Which is also my hobby"/>
+                <TimelineItem title="Intern"
+                date = "2024 - 2025" 
+                description = "Doing Internship as web developer in Essence Technology & Automation"/>
+              
+              <TimelineItem title="Full Stack Web Developer"
+                date = "2025 - Present" 
+                description = "Working as full stack web developer in Essence Technology & Automation"/>
             </ol>
         </div>
 
         <div className='skill'>
             <h3 className='h3 skills-title'>My Skills</h3>
             <ul className='skills-list content-card'>
-                <SkillItem title="Web Developement"  value={90} />
+                <SkillItem title="Web Developement(MERN)"  value={97} />
+                <SkillItem title="Web Developement(MEAN)"  value={95} />
+                <SkillItem title="MYSQL,MONGODB"  value={90} />
                 <SkillItem title="HTML,CSS,BootStrap,JS"  value={95} />
                 <SkillItem title="JAVA"  value={70} />
             </ul>
